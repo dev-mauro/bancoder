c4232fd04ce7006d918fd9b0223c86078e2de970
mauro-dev <silvac.mauro@gmail.com>
2022-09-25 21:49:49 -0300

    Implementacion uso de localStorage para guardar datos de la cuenta

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 class bankAccount {
-    constructor(balance = 0) {
-        this.accountNumber = createRandomAccNumber();
-        this.balance = balance;
+    constructor(base = {accountNumber: createRandomAccNumber(), balance: 0, movementHistory: []}) {
+        this.accountNumber = base.accountNumber;
+        this.balance = base.balance;
+        this.movementHistory = base.movementHistory;
         this.balanceView = document.getElementById('saldo');
-        this.movementHistory = [];
+
         this.setAccountNumber();
+        this.updateBalance();
+        this.updateHistory();
 
         this.newerFirst = true;
     }
@@ -16,12 +19,15 @@ class bankAccount {
     }
 
     addFunds(money = 0) {
-        if(isValidQuantity(money)){
-            this.addMovement(true, money);
-            this.balance += Number(money);
+        if(!isValidQuantity(money)){
+            return;
         }
 
+        this.addMovement(true, money);
+        this.balance += Number(money);
         this.updateBalance();
+
+        this.updateLocalStorage();
     }
 
     withdrawFunds(money = 0) {
@@ -36,6 +42,8 @@ class bankAccount {
         this.addMovement(false, money);
         this.balance -= Number(money);
         this.updateBalance();
+
+        this.updateLocalStorage();
     }
 
     updateBalance() {
@@ -65,13 +73,32 @@ class bankAccount {
         historyElement.innerHTML = '';
 
         for(let movement of history){
-            historyElement.appendChild(movement.htmlElement);
+            historyElement.innerHTML += movement.htmlElement;
         }
     }
 
     showWarning() {
         const aviso = document.getElementById('aviso');
         aviso.classList.remove('hidde');
+    }
+
+    updateLocalStorage() {
+        let accountIndex = 0;
+
+        //Si existen datos en localStorage, se busca el indice de esta cuenta bancaria
+        if(localData){
+            accountIndex = localData.bankAccounts.findIndex(account => account.accountNumber == this.accountNumber);
+        } else {
+            localData = {bankAccounts: []};
+        }
+
+        localData.bankAccounts[accountIndex] = { accountNumber: this.accountNumber,
+            movementHistory: this.movementHistory,
+            balance: this.balance
+        }
+
+        const JSONData = JSON.stringify(localData);
+        localStorage.setItem("bankAccounts", JSONData);
     }
 }
 
@@ -81,7 +108,7 @@ class Movement {
     //quntity = cantidad ingresada, add = true si se agregan fondos, false caso contrario
     constructor(bankAccount,  quantity, add = true) {
         this.initialBalance = bankAccount.balance;
-        this.date = new Date().toDateString();
+        this.date = this.getTime();
         this.quantity = quantity;
         
         let aux = 1; 
@@ -95,9 +122,6 @@ class Movement {
 
     buildHTMLElement(add) {
         // Fecha -> Descripción -> Monto inicial -> valor -> monto final
-        let htmlElement = document.createElement('div');
-        htmlElement.classList.add('movement');
-
         let movementType = "Abono";
         let symbol = '+';
         if(!add){
@@ -105,15 +129,26 @@ class Movement {
             movementType = "Retiro";
         }
 
-        htmlElement.innerHTML = `
-        <div class="movement-date">${this.date}</div>
-        <div class="movement-description">${movementType}</div>
-        <div class="movement-initial">$${this.initialBalance}</div>
-        <div class="movement-quantity"><span class="main-green">${symbol}</span>$${this.quantity}</div>
-        <div class="movement-final">$${this.finalBalance}</div>
+        const htmlElement = `
+        <div class="movement">
+            <div class="movement-date">${this.date.day}<span class="main-green">${this.date.time}</span></div>
+            <div class="movement-description">${movementType}</div>
+            <div class="movement-initial">$${this.initialBalance}</div>
+            <div class="movement-quantity"><span class="main-green">${symbol}</span>$${this.quantity}</div>
+            <div class="movement-final">$${this.finalBalance}</div>
+        </div>
         `;
 
         return htmlElement;
+    }
+
+    getTime() {
+        const date = new Date();
+
+        const time = date.toTimeString().slice(0, 5);
+        const day = date.toDateString().slice(4);
+
+        return {day: day, time: time};
     }
 }
 
@@ -136,7 +171,15 @@ const isValidQuantity = (quantity) => {
 
 //----------------------------------------
 
-const account = new bankAccount();
+let localData = JSON.parse(localStorage.getItem("bankAccounts"));
+// valor inicial para iniciar bankAccount
+let base = {accountNumber: createRandomAccNumber(), balance: 0, movementHistory: []};
+
+if(localData) {
+    base = localData.bankAccounts[0];
+}
+
+const account = new bankAccount(base);
 
 const bankPanel = {
     addInput: document.getElementById('inputDeposito'), 
@@ -179,7 +222,7 @@ warningButton.addEventListener('click', () => {
     aviso.classList.add('hidde');
 });
 
-//
+// Evento para cambiar orden de los movimientos
 orderSwapButton.addEventListener('click', () => {
     account.newerFirst = !(account.newerFirst);
 
